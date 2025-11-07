babb1f987125669b3a527c7c104d05c6ebc621b1
sushma <sushma@DESKTOP-T6A568G>
2025-11-07 14:28:41 +0530

    TC Created by sushama

---
+++ src/main/java/Ranjeet/SushamaDemo1.java
@@ -0,0 +1,11 @@
+package Ranjeet;
+
+public class SushamaDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Demo1 TC Created by sushama");
+	}
+
+}
