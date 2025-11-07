a8712b34dcfc494f893b7a17df07ef6a9da53f73
ranjeet <ramjeetp.mum@gmail.com>
2025-11-07 14:16:16 +0530

    Framework is created by ranjeet

---
+++ src/main/java/Ranjeet/ranjeetClass.java
@@ -0,0 +1,10 @@
+package Ranjeet;
+
+public class ranjeetClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Framework is creayed by ranjeet");
+	}
+
+}
